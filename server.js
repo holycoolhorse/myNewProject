67895d69db394080f521026f1488b855be97b677
mehmetandkoc <mehmetandkoc@gmail.com>
2025-01-13 23:36:19 +0300

    denem

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // bcrypt yerine bcryptjs kullanıyoruz
 const app = express();
 const port = process.env.PORT || 3000;
 
